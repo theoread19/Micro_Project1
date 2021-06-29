c219ec3f2935735ea74038fd94fee207a18a5e70
htloc <lochatan19@gmail.com>
2021-06-29 13:49:38 +0700

    model

---
+++ Domain/Models/AppsettingModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class AppsettingModel
+    {
+        public string DefaultConnection { get; set; }
+    }
+}
---
+++ Domain/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class UserModel
+    {
+        public long Id { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+
+    }
+}
