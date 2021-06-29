5da1ce63086dc0ac5e9de87432d0a679f07aeb6e
htloc <lochatan19@gmail.com>
2021-06-29 13:49:28 +0700

    interface repository

---
+++ Domain/Repository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Repository
+{
+    public interface IUserRepository
+    {
+        public List<UserModel> GetAll();
+        public UserModel GetById(long Id);
+        public void Insert(UserModel userModel);
+        public void Update(UserModel userModel);
+        public void Delete(long id);
+        public void SaveChange();
+    }
+}
