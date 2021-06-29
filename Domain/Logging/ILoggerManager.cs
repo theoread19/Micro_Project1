a195a9fb537b7452be9b89e9bce338a1d66b7b24
htloc <lochatan19@gmail.com>
2021-06-29 13:49:14 +0700

    interface logging

---
+++ Domain/Logging/ILoggerManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Logging
+{
+    public interface ILoggerManager
+    {
+        public void LogInfo(string message);
+        public void LogWarn(string message);
+        public void LogDebug(string message);
+        public void LogError(string message);
+    }
+}
