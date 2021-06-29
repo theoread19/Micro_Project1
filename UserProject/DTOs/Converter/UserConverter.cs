18c3563a433d6a89e91468d31632247e70dae6a9
htloc <lochatan19@gmail.com>
2021-06-29 13:46:06 +0700

    convert bwt req vs model object

---
+++ UserProject/DTOs/Converter/UserConverter.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserProject.DTOs.Request;
+
+namespace UserProject.DTOs.Converter
+{
+    public class UserConverter
+    {
+        public UserRequest toReq(UserModel model)
+        {
+            UserRequest req = new UserRequest();
+            req.Id = model.Id;
+            req.Fullname = model.Fullname;
+            req.Email = model.Email;
+            return req;
+        }
+
+        public UserModel toModel(UserRequest req)
+        {
+            UserModel model = new UserModel();
+            model.Fullname = req.Fullname;
+            model.Email = req.Email;
+            return model;
+        }
+
+        public UserModel toModel(UserRequest req, ref UserModel model)
+        {
+            model.Id = req.Id;
+            model.Fullname = req.Fullname;
+            model.Email = req.Email;
+            return model;
+        }
+    }
+}
