221a2821555781d6e96621f243c50c1dd417fb2d
htloc <lochatan19@gmail.com>
2021-06-29 13:46:42 +0700

    API/Host/WEB API

---
+++ UserProject/Startup.cs
@@ -0,0 +1,93 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Domain.Logging;
+using Domain.Repository;
+using Infrastructure.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserProject.Services;
+using UserProject.Services.iplm;
+using Infrastructure.Logging;
+using Domain.Models;
+
+namespace UserProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IContainer ApplicationContainer { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen();
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+
+            this.ApplicationContainer = builder.Build();
+            //repository
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            //service
+            services.AddScoped<IUserService, UserService>();
+            //
+
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+
+            builder.RegisterType<UserRepository>().As<IUserService>();
+            builder.RegisterType<UserService>().As<IUserService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger(c =>
+            {
+                c.SerializeAsV2 = true;
+            });
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),  
+            // specifying the Swagger JSON endpoint.  
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
