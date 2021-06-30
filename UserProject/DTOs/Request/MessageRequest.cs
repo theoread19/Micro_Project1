010fdc09d610a995324955ca7d4e9a767098c8f5
htloc <lochatan19@gmail.com>
2021-06-30 14:42:46 +0700

    message request

---
+++ UserProject/DTOs/Request/MessageRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserProject.DTOs.Request
+{
+    public class MessageRequest
+    {
+        public long id { get; set; }
+        public string content { get; set; }
+        public string title { get; set; }
+        public long senderId { get; set; }
+        public long recipientId { get; set; }
+        public DateTime createDate { get; set; }
+    }
+}
